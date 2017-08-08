0d0d87918b789c175e5c8497cc74b0b50109e212
Renan Le Gall <renan.legall@ieseg.fr>
2017-08-08 14:36:21 +0200

    add model Article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10 times do {
+  title = Faker::HarryPotter.character
+  content = Faker::HarryPotter.quote
+  art = Article.new(title: title, content: content)
+  art.save
+}
+
+end
